54d95c21d294a5e1a8548b3b9968f75c49e436ed
Ryuseless <janfilipzacek@seznam.cz>
2023-09-30 15:01:14 +0200

    added first row of opt buttons

--- sgive/src/gui_template/guiTemplate.py
+++ sgive/src/gui_template/guiTemplate.py
@@ -6,6 +6,14 @@ from tkinter import *
 from tkinter import font
 import configActions as JS
 
+def getButtonConf():
+    # collecting values for font and colors
+    bg = JS.jsonRed('colors_info', "buttons_unselected")
+    bg_active = JS.jsonRed('colors_info', "buttons_selected")
+    fontFamily = JS.jsonRed('font_info', "family")
+    fontSize = JS.jsonRed('font_info', "size")
+    # end of collecting values for font and colors
+    return bg, bg_active, fontFamily, fontSize
 
 def resolutionMath(root: tkinter.Tk):
     _screenWidth = root.winfo_screenwidth()
@@ -20,11 +28,9 @@ def resolutionMath(root: tkinter.Tk):
 
 
 class _MenuFrameTemplate:
-    def __init__(self, root: tkinter.Tk):
+    def __init__(self, root: tkinter.Tk,sixWidth: int, sixHeight: int):
         self.root = root
         bg_color = '#e5e5e5'
-        sixWidth = resolutionMath(root)[1]
-        sixHeight = resolutionMath(root)[2]
 
         # master frame
         master_bar = Frame(root, height=sixHeight, bg=bg_color)
@@ -33,37 +39,37 @@ class _MenuFrameTemplate:
         master_bar.pack(side=TOP, fill=X)
 
         # menu frame
-        menu_bar = Frame(master_bar, width=sixWidth, bg=bg_color)
-        menu_bar.pack_propagate(False)
-        menu_bar.pack(side=LEFT, fill=Y)
+        self.menuBar = Frame(master_bar, width=sixWidth, bg=bg_color)
+        self.menuBar.pack_propagate(False)
+        self.menuBar.pack(side=LEFT, fill=Y)
 
-        # exit frame
-        exit_bar = Frame(master_bar, width=sixWidth, bg=bg_color)
-        exit_bar.pack_propagate(False)
-        exit_bar.pack(side=RIGHT, fill=Y)
+        # back frame
+        self.backBar = Frame(master_bar, width=sixWidth, bg=bg_color)
+        self.backBar.pack_propagate(False)
+        self.backBar.pack(side=RIGHT, fill=Y)
 
         # options frame
-        options_bar = Frame(master_bar, bg=bg_color)
-        options_bar.pack_propagate(False)
-        options_bar.pack(expand=True, fill=BOTH)
-
-        MenuFrameCreateButtons(menu_bar, exit_bar, sixWidth, sixHeight)
-
+        self.optionsBar = Frame(master_bar, bg=bg_color)
+        self.optionsBar.pack_propagate(False)
+        self.optionsBar.pack(expand=True, fill=BOTH)
 
 class MenuFrameCreateButtons:
     """Create Menu_Action_Buttons and Back_Action_Button.
     :param text_value: this class creates buttons for menu selection and going back option.
     """
-    def __init__(self, menu_bar: tkinter.Frame, exit_bar: tkinter.Frame, sixWidth: int, sixHeight: int):
-        self.sixWidth = sixWidth            # get portion of the screen width
-        self.sixHeight = sixHeight          # get portion of the screen height
-        self.menu_bar = menu_bar            # tkinter menu frame
-        self.exit_bar = exit_bar            # tkinter back frame
-        self.button_dict = {}               # this thing for creating menu and back buttons
-        self.option = []                    # array for specifying 'IDs' for buttons
-        self.id_of_menu = 1                 # value that keeps track of which ID is in use
-        self.createOptArr()                 # get number of men. and bac. buttons
-        self.createMenuAndBackButtons()     # def call
+    def __init__(self, menu_bar: tkinter.Frame, exit_bar: tkinter.Frame,options_bar: tkinter.Frame, sixWidth: int, sixHeight: int):
+        self.sixWidth = sixWidth  # get portion of the screen width
+        self.optionsBar = options_bar
+        self.sixHeight = sixHeight  # get portion of the screen height
+        self.menu_bar = menu_bar  # tkinter menu frame
+        self.exit_bar = exit_bar  # tkinter back frame
+        self.button_dict = {}  # this thing for creating menu and back buttons
+        self.option = []  # array for specifying 'IDs' for buttons
+        self.id_of_menu = 1  # value that keeps track of which ID is in use
+        self.createOptArr()  # get number of men. and bac. buttons
+        self.createMenuAndBackButtons()  # def call
+        #call for opt. class buttons:
+        self.optButtons = optFrameCreateButtons(optionsBar=options_bar,sixWidth=sixWidth,sixHeight=sixHeight)
 
     def createOptArr(self):  # this reads values from conf.json and creates array based of length that was given
         _numberOfValues = JS.jsonRed('buttons_info', "num_of_act_buttons")
@@ -73,24 +79,29 @@ class MenuFrameCreateButtons:
             _counter += 1
 
     def menuActionUp(self):  # this def goes up one menu button: menu_X -> menu_X+1
+        if self.id_of_menu == 1:
+            new_dict = self.optButtons.button_dict
+            for i in new_dict:
+                new_dict[i].pack_forget()
         # here is 'len(self.option) - 1', because the last one is back button
         if not self.id_of_menu >= len(self.option) - 1:
             self.button_dict[self.id_of_menu].pack_forget()
             self.button_dict[self.id_of_menu + 1].pack()
             self.id_of_menu += 1
 
-    def menuActionDown(self):    # this def goes back one menu button: menu_X -> menu_X-1
+    def menuActionDown(self):  # this def goes back one menu button: menu_X -> menu_X-1
         if not self.id_of_menu == 1:
             self.button_dict[self.id_of_menu].pack_forget()
             self.button_dict[self.id_of_menu - 1].pack()
             self.id_of_menu -= 1
 
-    def createMenuAndBackButtons(self):    # this def creates menu and back action button
-        # collecting values for font and colors
-        bg = JS.jsonRed('colors_info', "buttons_unselected")
-        bg_active = JS.jsonRed('colors_info', "buttons_selected")
-        _fontFamily = JS.jsonRed('font_info', "family")
-        _fontSize = JS.jsonRed('font_info', "size")
+    def createMenuAndBackButtons(self):  # this def creates menu and back action button
+        # collecting values for font and colors from def
+        getValues = getButtonConf()
+        bg = getValues[0]
+        bg_active = getValues[1]
+        _fontFamily = getValues[2]
+        _fontSize = getValues[3]
         fontInfo = font.Font(family=_fontFamily, size=_fontSize, weight=font.BOLD)
         # end of collecting values for font and colors
         for i in self.option:
@@ -109,6 +120,48 @@ class MenuFrameCreateButtons:
         # show first menu and back button
         self.button_dict[1].pack()  # FIRST MENU
         self.button_dict[len(self.option)].pack()  # BACK (is the last button)
+
+
+class optFrameCreateButtons:
+    def __init__(self, optionsBar: tkinter.Frame, sixWidth: int, sixHeight: int):
+        self.optionsBar = optionsBar
+        self.button_dict = {}  # this thing for creating menu and back buttons
+        self.option = []  # array for specifying 'IDs' for buttons
+        self.sixWidth = sixWidth  # get portion of the screen width
+        self.sixHeight = sixHeight  # get portion of the screen height
+        self.createOptArr()
+        self.createOPT1Buttons()
+
+    def createOptArr(self):  # this reads values from conf.json and creates array based of length that was given
+        _numberOfValues = JS.jsonRed('buttons_info', "num_of_opt_on_frame")
+        _counter = 1
+        while _counter <= _numberOfValues:
+            self.option.append(_counter)
+            _counter += 1
+
+    def getAway(self):
+        for i in self.option:
+            self.button_dict[i].pack_forget()
+
+    def createOPT1Buttons(self):
+        # collecting values for font and colors from def
+        getValues = getButtonConf()
+        bg = getValues[0]
+        bg_active = getValues[1]
+        _fontFamily = getValues[2]
+        _fontSize = getValues[3]
+        fontInfo = font.Font(family=_fontFamily, size=_fontSize, weight=font.BOLD)
+        # end of collecting values for font and colors
+        for i in self.option:
+            self.button_dict[i] = Button(self.optionsBar, text=f'OPT{i}',command= lambda : self.getAway())
+            self.button_dict[i]['activebackground'] = bg_active
+            self.button_dict[i]['bg'] = bg
+            self.button_dict[i]['font'] = fontInfo
+            self.button_dict[i]['height'] = self.sixHeight
+            self.button_dict[i]['borderwidth'] = 2
+            self.button_dict[i]['relief'] = 'solid'
+            # show buttons
+            self.button_dict[i].pack(side=LEFT, padx=10)
 
 
 class Application_frame_temp:
@@ -134,7 +187,14 @@ class Application_frame_temp:
 
 class App:
     def __init__(self, root: tkinter.Tk):
-        self.root = root
+        sixWidth = resolutionMath(root)[1]  # get sixth of the resolution to make frame width
+        sixHeight = resolutionMath(root)[2]  # get sixth of the resolution to make frame height
         root.title("SeniorOS interface app")
-        root.attributes('-fullscreen', True)
-        _MenuFrameTemplate(root)
+        root.attributes('-fullscreen', True) # make app fullscreen
+        # calling classes
+        menuFrameTemp = _MenuFrameTemplate(root, sixWidth, sixHeight)
+        self.menuFrameTempVal = menuFrameTemp.optionsBar #return class frame value
+        menuFrameCreateButtons = MenuFrameCreateButtons(menuFrameTemp.menuBar, menuFrameTemp.backBar, menuFrameTemp.optionsBar, sixWidth, sixHeight)
+        self.menuFrameCreateButtonsVal = menuFrameCreateButtons
+
+
--- sgive/src/gui_template/main.py
+++ sgive/src/gui_template/main.py
@@ -1,13 +1,21 @@
+from pprint import pprint
 from tkinter import *
 import guiTemplate as temp
 import configActions as act
 
+
 if __name__ == '__main__':
+    root = Tk()
+    value = temp.App(root)
+    print("value jest:",value.menuFrameTempVal)
+
+    root.mainloop()
+    """
     confCheck = act.configExistCheck()
     if confCheck:
-        print("main")
         root = Tk()
         temp.App(root)
-        idk = temp.Application_frame_temp(root)
-        idk.create_exit_button("EXIT")
-        root.mainloop()
+
+
+
+    """
