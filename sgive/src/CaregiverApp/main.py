c022d64e41181f1cfc272625b515369f5f2df929
Ryuseless <janfilipzacek@seznam.cz>
2023-10-16 19:58:43 +0200

    updates.

--- sgive/src/CaregiverApp/main.py
+++ sgive/src/CaregiverApp/main.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     split = whereTheFuckAmI.split("sgive")
     path = split[0]
     configPath = os.path.join(path, "sconf")
-    #create config, only if there is not any config.json already
+    # create config, only if there is not any config.json already
     if os.path.exists(configPath) and not os.path.isfile(os.path.join(configPath, 'config.json')):
         ryuconf.caregiverAppConfig(configPath)
         logging.warning("No config was found, generating new one.")
